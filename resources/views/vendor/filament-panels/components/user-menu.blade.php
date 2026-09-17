17596954f040ef2682fc90802f9e107e34692a1d
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-17 21:55:55 +0700

    feat: enhance logout modal with user details and improved layout

--- resources/views/vendor/filament-panels/components/user-menu.blade.php
+++ resources/views/vendor/filament-panels/components/user-menu.blade.php
@@ -75,32 +75,47 @@ $items = \Illuminate\Support\Arr::except($items, ['account', 'logout', 'profile'
     </x-filament::dropdown.list>
 </x-filament::dropdown>
 
-<x-filament::modal id="logout-modal" width="md" alignment="center">
-    <x-slot name="heading">
-        <div class="w-full text-center">Confirm Logout</div>
-    </x-slot>
+<x-filament::modal id="logout-modal" width="md" alignment="center" :close-by-clicking-away="false"
+        icon="heroicon-o-arrow-right-start-on-rectangle" icon-color="danger">
+        <x-slot name="heading">
+            Confirm Logout
+        </x-slot>
 
-    <x-slot name="description">
-        <div class="w-full text-center">Are you sure you want to log out of the application?</div>
-    </x-slot>
+        <x-slot name="description">
+            <div class="space-y-3">
+                <p class="text-center text-sm text-gray-500 dark:text-gray-400">You are about to log out from your account.</p>
+                <div class="mx-auto flex items-center gap-3 rounded-xl bg-gray-50 px-4 py-3 dark:bg-white/5">
+                    <div class="flex h-9 w-9 shrink-0 items-center justify-center rounded-full bg-[#8B4513] text-sm font-semibold text-white">
+                        {{ strtoupper(substr($user->name, 0, 1)) }}
+                    </div>
+                    <div class="min-w-0 text-left">
+                        <p class="truncate text-sm font-medium text-gray-900 dark:text-white">{{ $user->name }}</p>
+                        <p class="truncate text-xs text-gray-500 dark:text-gray-400">{{ $user->email }}</p>
+                    </div>
+                </div>
+            </div>
+        </x-slot>
 
-    <x-slot name="footerActions">
-        <x-filament::button color="gray" x-on:click="$dispatch('close-modal', { id: 'logout-modal' })">
-            Cancel
-        </x-filament::button>
-        <form method="post" action="{{ $logoutItem?->getUrl() ?? filament()->getLogoutUrl() }}"
-            x-data="{ loading: false }" @submit="loading = true">
-            @csrf
-            <x-filament::button color="danger" type="submit" x-bind:disabled="loading"
-                x-bind:class="loading ? 'cursor-wait opacity-70' : ''">
-                <span x-show="! loading">Logout</span>
-                <span x-show="loading" class="flex items-center gap-2">
-                    <x-heroicon-m-arrow-path class="h-4 w-4 animate-spin text-white" />
-                    Logging out...
-                </span>
+        <x-slot name="footerActions" class="flex justify-center gap-3">
+            <x-filament::button color="gray" x-on:click="$dispatch('close-modal', { id: 'logout-modal' })">
+                Cancel
             </x-filament::button>
-        </form>
-    </x-slot>
-</x-filament::modal>
+            <form method="post" action="{{ $logoutItem?->getUrl() ?? filament()->getLogoutUrl() }}"
+                x-data="{ loading: false }" @submit="loading = true" class="contents">
+                @csrf
+                <x-filament::button color="danger" type="submit" x-bind:disabled="loading"
+                    x-bind:class="loading ? 'cursor-wait opacity-70' : ''">
+                    <span x-show="! loading" class="flex items-center gap-2">
+                        <x-heroicon-o-arrow-right-start-on-rectangle class="h-4 w-4" />
+                        Logout
+                    </span>
+                    <span x-show="loading" class="flex items-center gap-2">
+                        <x-heroicon-m-arrow-path class="h-4 w-4 animate-spin text-white" />
+                        Logging out...
+                    </span>
+                </x-filament::button>
+            </form>
+        </x-slot>
+    </x-filament::modal>
 
 {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::USER_MENU_AFTER) }}

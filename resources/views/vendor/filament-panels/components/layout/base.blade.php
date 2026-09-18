fe4802754c202c3025544c517affa1ae696792d2
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-19 00:45:22 +0700

    fix: update favicon link to use asset helper for proper asset management

--- resources/views/vendor/filament-panels/components/layout/base.blade.php
+++ resources/views/vendor/filament-panels/components/layout/base.blade.php
@@ -17,7 +17,7 @@ filament()->hasDarkModeForced(),
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
 
     @php
     $title = trim(strip_tags(($livewire ?? null)?->getTitle() ?? ''));

3179de2499552d119cad6577c1d2b8aa1346c034
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-22 00:57:09 +0700

    refactor: remove ProjectDetail page and associated view; clean up unused code

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(
